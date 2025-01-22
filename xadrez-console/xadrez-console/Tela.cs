34fe392c05c953afb4267dc00ee1ae58043a040c
Bruno Eiky Yamamoto <brunoeiky2006@gmail.com>
2025-01-22 19:15:08 -0300

    Melhorando a impressao do tabuleiro

--- xadrez-console/xadrez-console/Tela.cs
+++ xadrez-console/xadrez-console/Tela.cs
@@ -7,17 +7,32 @@ namespace xadrez_console
         public static void imprimirTabuleiro (Tabuleiro Tabuleiro)
         {
             for (int l = 0; l < Tabuleiro.Linhas; l++) {
+               Console.Write(8-l + " ");
                for (int c = 0; c < Tabuleiro.Colunas; c++){
                     if (Tabuleiro.LocalPeca(l,c) == null){
                         Console.Write("-" + " ");
                     }
                     else {
-                        Console.Write(Tabuleiro.LocalPeca(l, c) + " ");
+                        ImprimirPeca(Tabuleiro.LocalPeca(l, c));
+                        Console.Write(" ");
                     }
                }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
 
+        public static void ImprimirPeca (Peca peca)
+        {
+            if (peca.Cor == Cor.Branca) {
+                Console.Write(peca);
+            }
+            else{
+                ConsoleColor Atual = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = Atual;
+            }
         }
     }
 }
